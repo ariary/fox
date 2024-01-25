461b1a3757d3bde893836066ce39543992a9668d
ariary <ariary9.2@hotmail.fr>
2024-01-25 09:59:55 +0100

    parse cmdline for send

--- Makefile
+++ Makefile
@@ -3,4 +3,6 @@ build.fox:
 buildAndRun.fox.receive:
 	nim -o:bin/fox c -r src/fox.nim
 buildAndRun.fox.send:
+	nim -o:bin/fox c -r src/fox.nim ../fox/LICENSE
+buildAndRun.fox.send2:
 	cat LICENSE | nim -o:bin/fox c -r src/fox.nim
--- src/fox.nim
+++ src/fox.nim
@@ -1,13 +1,29 @@
 import terminal
+import os
+import system
+import std/strutils
 
-if isatty(stdin):
-  # receive
-  # Launch server
-  styledEcho styleBright, Green, "🦊 received!"
+let args = commandLineParams()
+var sendFilename: string
+if len(args)>0:
+  if not fileExists(args[0]):
+    echo args[0], " is not a file!"
+    quit(92)
+  else:
+    sendFilename = args[0].split('/')[^1]
 else:
+  if not isatty(stdin):
+    sendFilename = "fox"
+  
+
+if sendFilename != "":
   # send
   # Launch server
-  styledEcho styleBright, bgCyan, "🦊 sent!"
+  styledEcho sendFilename, styleBright, fgCyan, " 🦊 sent!", resetStyle
+else:
+  # receive
+  # Launch server
+  styledEcho styleBright, fgGreen, "🦊 received!",resetStyle
 
 
 # Server function start a server and stop after first request received
